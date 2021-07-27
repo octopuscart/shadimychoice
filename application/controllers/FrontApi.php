4106eccc428d5ccc40bffcb6edb93a147c55f69b
octopuscart <dev@develop.com>
2021-07-27 09:48:29 +0530

    cahgns

--- application/controllers/FrontApi.php
+++ application/controllers/FrontApi.php
@@ -470,7 +470,7 @@ class FrontApi extends REST_Controller {
         $userdata = $query->row();
 
         if ($userdata) {
-            $this->db->where("user_id", $restul->id);
+            $this->db->where("user_id", $memberdata->id);
             $query = $this->db->get("shadi_profile");
             $memberdata = $query->row();
             $userdata->member_profile_id = $memberdata->member_id;
